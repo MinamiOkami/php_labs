1aca29aa032909d1ec72388cf770a47868a365fa
Елизавета Колушова <taeknork@gmail.com>
2022-06-14 11:04:22 +0300

    2.1

---
+++ OOP/db-blog.sql
@@ -0,0 +1,146 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Хост: 127.0.0.1
+-- Время создания: Июн 09 2022 г., 20:51
+-- Версия сервера: 10.4.22-MariaDB
+-- Версия PHP: 8.1.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `db-blog`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `articles`
+--
+
+CREATE TABLE `articles` (
+  `id` int(11) NOT NULL,
+  `author_id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `text` text NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `articles`
+--
+
+INSERT INTO `articles` (`id`, `author_id`, `name`, `text`, `created_at`) VALUES
+(1, 1, ' Статья №1', ' Можно взять что-то вроде Lorem Ipsum', '2022-06-07 11:02:38'),
+(2, 1, ' Статья №2', 'Можно взять что-то вроде Lorem Ipsum ', '2022-06-07 11:02:38');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `comments`
+--
+
+CREATE TABLE `comments` (
+  `id` int(11) NOT NULL,
+  `author_id` int(11) NOT NULL,
+  `article_id` int(11) NOT NULL,
+  `text` text NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `comments`
+--
+
+INSERT INTO `comments` (`id`, `author_id`, `article_id`, `text`, `created_at`) VALUES
+(1, 1, 1, 'Огонёк', '2022-06-07 11:26:29'),
+(2, 1, 1, 'Test Test', '2022-06-07 11:33:26'),
+(3, 1, 2, 'sdsad', '2022-06-07 11:58:15'),
+(6, 1, 1, 'my test', '2022-06-07 12:20:51'),
+(7, 1, 1, 'jgjgjва', '2022-06-07 12:44:52'),
+(11, 1, 1, 'ftftftftftf', '2022-06-07 13:15:05');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `nickname` varchar(128) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `is_confirmed` tinyint(1) NOT NULL DEFAULT 0,
+  `role` enum('admin','user') NOT NULL,
+  `password_hash` varchar(255) NOT NULL,
+  `auth_token` varchar(255) NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `users`
+--
+
+INSERT INTO `users` (`id`, `nickname`, `email`, `is_confirmed`, `role`, `password_hash`, `auth_token`, `created_at`) VALUES
+(1, 'admin', 'admin@gmail.com', 1, 'admin', 'hash1', 'token1', '2022-06-07 11:02:12'),
+(2, 'user', 'user@gmail.com', 1, 'user', 'hash2', 'token2', '2022-06-07 11:02:13');
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `articles`
+--
+ALTER TABLE `articles`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `comments`
+--
+ALTER TABLE `comments`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `nickname` (`nickname`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `articles`
+--
+ALTER TABLE `articles`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT для таблицы `comments`
+--
+ALTER TABLE `comments`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT для таблицы `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ OOP/templates/main/bye.php
@@ -0,0 +1,3 @@
+<?php include __DIR__.'/../header.html';?> 
+    <p>Пока, <?= $name;?>!!!</p>
+<?php include __DIR__.'/../footer.html';?> 
